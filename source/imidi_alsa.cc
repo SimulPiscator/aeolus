SimulPiscator/aeolus

commit 16c798cfadb95c6e4cd52f40c52bcf8977991832

Factor out imidi class into alsa and coremidi elements

source/imidi_alsa.cc
@@ -1,0 +1,126 @@
+// ----------------------------------------------------------------------------
+//
+//  Copyright (C) 2003-2013 Fons Adriaensen <fons@linuxaudio.org>
+//    
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// ----------------------------------------------------------------------------
+
+
+#include "imidi_alsa.h"
+
+Imidi_alsa::Imidi_alsa (Lfq_u32 *qnote, Lfq_u8 *qmidi, uint16_t *midimap, const char *appname) : Imidi(midimap, appname),
+    _handle(NULL)
+{
+}
+
+void Imidi_alsa::on_terminate (void)
+{
+    snd_seq_event_t E;
+
+    if (_handle)
+    {   
+	snd_seq_ev_clear (&E);
+	snd_seq_ev_set_direct (&E);
+	E.type = SND_SEQ_EVENT_USR0;
+	E.source.port = _opport;
+	E.dest.client = _client;
+	E.dest.port   = _ipport;
+	snd_seq_event_output_direct (_handle, &E);
+    }
+}
+
+
+void Imidi_alsa::thr_main (void)
+{
+    open_midi ();
+    proc_midi ();
+    close_midi ();
+    send_event (EV_EXIT, 1);
+}
+
+
+void Imidi_alsa::on_open_midi (void)
+{
+    snd_seq_client_info_t *C;
+
+    if (snd_seq_open (&_handle, "hw", SND_SEQ_OPEN_DUPLEX, 0) < 0)
+    {
+        fprintf(stderr, "Error opening ALSA sequencer.\n");
+        exit(1);
+    }
+
+    snd_seq_client_info_alloca (&C);
+    snd_seq_get_client_info (_handle, C);
+    _client = snd_seq_client_info_get_client (C);
+    snd_seq_client_info_set_name (C, _appname);
+    snd_seq_set_client_info (_handle, C);
+
+    if ((_ipport = snd_seq_create_simple_port (_handle, "In",
+        SND_SEQ_PORT_CAP_WRITE | SND_SEQ_PORT_CAP_SUBS_WRITE,
+        SND_SEQ_PORT_TYPE_APPLICATION)) < 0)
+    {
+        fprintf(stderr, "Error creating sequencer input port.\n");
+        exit(1);
+    }
+
+    if ((_opport = snd_seq_create_simple_port (_handle, "Out",
+         SND_SEQ_PORT_CAP_WRITE,
+         SND_SEQ_PORT_TYPE_APPLICATION)) < 0)
+    {
+        fprintf(stderr, "Error creating sequencer output port.\n");
+        exit(1);
+    }
+}
+
+
+void Imidi_alsa::on_close_midi (void)
+{
+    if (_handle) snd_seq_close (_handle);
+}
+
+void Imidi_alsa::proc_midi (void)
+{
+    // Read and process MIDI commands from the ALSA port.
+    // Events related to keyboard state are sent to the 
+    // audio thread via the qnote queue. All the rest is
+    // sent as raw MIDI to the model thread via qmidi.
+
+    while (true)
+    {
+        snd_seq_event_t  *E;
+	    snd_seq_event_input(_handle, &E);
+
+        if(E->type == SND_SEQ_EVENT_USR0)
+            // User event, terminates this trhead if we sent it.
+            if (E->source.client == _client) return;
+        }
+
+        MidiEvent ev = { 0 };
+        ev.type = E->type;
+        switch(ev.type) {
+            case SND_SEQ_EVENT_NOTEON:
+            case SND_SEQ_EVENT_NOTEOFF:
+                ev.note.channel = E->data.note.channel;
+                ev.note.note = E->data.note.note;
+                ev.note.velocity = E->data.note.velocity;
+                break;
+            case SND_SEQ_EVENT_PGMCHANGE:
+            case SND_SEQ_EVENT_CONTROLLER:
+                ev.control.param = E->data.control.param;
+                ev.control.value = E->data.control.value;
+                break;
+        }
+    }
+}

CMakeLists.txt
@@ -71,7 +71,7 @@ target_link_libraries(aeolus_x11
 )
 install(TARGETS aeolus_txt aeolus_x11 DESTINATION ${LIBDIR})
 
-add_executable(aeolus
+set(AEOLUS_SRC
   source/addsynth.cc
   source/addsynth.h
   source/asection.cc
@@ -103,6 +103,19 @@ add_executable(aeolus
   source/scales.h
   source/slave.cc
   source/slave.h
+)
+if(LINUX)
+    list(APPEND AEOLUS_SRC
+        source/imidi_alsa.cc
+    )
+elseif(APPLE)
+    list(APPEND AEOLUS_SRC
+        source/imidi_coremidi.cc
+    )
+endif()
+
+add_executable(aeolus
+  ${AEOLUS_SRC}
 )
 target_link_libraries(aeolus 
     ${JACK_LIBRARY}

source/imidi.cc
@@ -20,14 +20,14 @@
 
 #include "imidi.h"
 
-
-
 Imidi::Imidi (Lfq_u32 *qnote, Lfq_u8 *qmidi, uint16_t *midimap, const char *appname) :
     A_thread ("Imidi"),
-    _qnote (qnote),
-    _qmidi (qmidi),
+    _appname (appname),
+    _qnote(qnote),
+    _qmidi(qmidi),
     _midimap (midimap),
-    _appname (appname)
+    _client(NULL),
+    _ipport(0)
 {
 }
 
@@ -36,67 +36,11 @@ Imidi::~Imidi (void)
 {
 }
 
-
-void Imidi::terminate (void)
-{
-    snd_seq_event_t E;
-
-    if (_handle)
-    {   
-	snd_seq_ev_clear (&E);
-	snd_seq_ev_set_direct (&E);
-	E.type = SND_SEQ_EVENT_USR0;
-	E.source.port = _opport;
-	E.dest.client = _client;
-	E.dest.port   = _ipport;
-	snd_seq_event_output_direct (_handle, &E);
-    }
-}
-
-
-void Imidi::thr_main (void)
-{
-    open_midi ();
-    proc_midi ();
-    close_midi ();
-    send_event (EV_EXIT, 1);
-}
-
-
 void Imidi::open_midi (void)
 {
-    snd_seq_client_info_t *C;
-    M_midi_info *M;
+    on_open_midi();
 
-    if (snd_seq_open (&_handle, "hw", SND_SEQ_OPEN_DUPLEX, 0) < 0)
-    {
-        fprintf(stderr, "Error opening ALSA sequencer.\n");
-        exit(1);
-    }
-
-    snd_seq_client_info_alloca (&C);
-    snd_seq_get_client_info (_handle, C);
-    _client = snd_seq_client_info_get_client (C);
-    snd_seq_client_info_set_name (C, _appname);
-    snd_seq_set_client_info (_handle, C);
-
-    if ((_ipport = snd_seq_create_simple_port (_handle, "In",
-        SND_SEQ_PORT_CAP_WRITE | SND_SEQ_PORT_CAP_SUBS_WRITE,
-        SND_SEQ_PORT_TYPE_APPLICATION)) < 0)
-    {
-        fprintf(stderr, "Error creating sequencer input port.\n");
-        exit(1);
-    }
-
-    if ((_opport = snd_seq_create_simple_port (_handle, "Out",
-         SND_SEQ_PORT_CAP_WRITE,
-         SND_SEQ_PORT_TYPE_APPLICATION)) < 0)
-    {
-        fprintf(stderr, "Error creating sequencer output port.\n");
-        exit(1);
-    }
-
-    M = new M_midi_info ();
+    M_midi_info *M = new M_midi_info ();
     M->_client = _client;
     M->_ipport = _ipport;
     memcpy (M->_chbits, _midimap, 16 * sizeof (uint16_t));
@@ -106,35 +50,30 @@ void Imidi::open_midi (void)
 
 void Imidi::close_midi (void)
 {
-    if (_handle) snd_seq_close (_handle);
+    on_close_midi();
 }
 
-
-void Imidi::proc_midi (void) 
+void Imidi::terminate()
 {
-    snd_seq_event_t  *E;
-    int              c, f, m, n, p, t, v;
+    on_terminate();
+}
 
-    // Read and process MIDI commands from the ALSA port.
-    // Events related to keyboard state are sent to the 
-    // audio thread via the qnote queue. All the rest is
-    // sent as raw MIDI to the model thread via qmidi.
+void Imidi::proc_midi_event(const MidiEvent &ev)
+{
+    int              c, f, m, t, n, v, p;
 
-    while (true)
-    {
-	snd_seq_event_input(_handle, &E);
-        c = E->data.note.channel;               
-        m = _midimap [c] & 127;        // Keyboard and hold bits
+    c = ev.note.channel;
+    m = _midimap [c] & 127;        // Keyboard and hold bits
 //        d = (_midimap [c] >>  8) & 7;  // Division number if (f & 2)
-        f = (_midimap [c] >> 12) & 7;  // Control enabled if (f & 4)
+    f = (_midimap [c] >> 12) & 7;  // Control enabled if (f & 4)
 
-        t = E->type;
+    t = ev.type;
 	switch (t)
 	{ 
 	case SND_SEQ_EVENT_NOTEON:
 	case SND_SEQ_EVENT_NOTEOFF:
-	    n = E->data.note.note;
-	    v = E->data.note.velocity;
+	    n = ev.note.note;
+	    v = ev.note.velocity;
             if ((t == SND_SEQ_EVENT_NOTEON) && v)
 	    {
                 // Note on.
@@ -186,8 +125,8 @@ void Imidi::proc_midi (void)
 	    break;
 
 	case SND_SEQ_EVENT_CONTROLLER:
-	    p = E->data.control.param;
-	    v = E->data.control.value;
+	    p = ev.control.param;
+	    v = ev.control.value;
 	    switch (p)
 	    {
 	    case MIDICTL_HOLD:
@@ -270,17 +209,13 @@ void Imidi::proc_midi (void)
    	        if (_qmidi->write_avail () >= 3)
 	        {
 		    _qmidi->write (0, 0xC0);
-		    _qmidi->write (1, E->data.control.value);
+		    _qmidi->write (1, ev.control.value);
 		    _qmidi->write (2, 0);
 		    _qmidi->write_commit (3);
 		}
             }
 	    break;
 
-	case SND_SEQ_EVENT_USR0:
-	    // User event, terminates this trhead if we sent it.
-	    if (E->source.client == _client) return;
-	}
     }
 }
 

source/imidi.h
@@ -25,16 +25,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <clthreads.h>
-#ifdef __linux__
-#include <alsa/asoundlib.h>
-#endif
-#ifdef __APPLE__
-#include <CoreMIDI/MIDIServices.h>
-#endif
 #include "lfqueue.h"
 #include "messages.h"
-
-
+#if __linux__
+# include <asound.h>
+#else
+enum {
+    SND_SEQ_EVENT_NOTEON = 1,
+    SND_SEQ_EVENT_NOTEOFF,
+    SND_SEQ_EVENT_CONTROLLER,
+    SND_SEQ_EVENT_PGMCHANGE,
+    SND_SEQ_EVENT_NONE
+};
+#endif
 
 class Imidi : public A_thread
 {
@@ -44,32 +47,33 @@ public:
     virtual ~Imidi (void);
 
     void terminate (void);
-#ifdef __APPLE__
-    void coremidi_proc (const MIDIPacketList *pktlist, void *refCon, void *connRefCon);
-#endif
-
-private:
-
-    virtual void thr_main (void);
+    
+protected:
+    struct MidiEvent
+    {
+        int type;
+        union {
+            struct { int channel, note, velocity; } note;
+            struct { int channel, param, value; } control;
+        };
+    };
+    void proc_midi_event(const MidiEvent&);
 
     void open_midi (void);
     void close_midi (void);
-    void proc_midi (void);
-    void proc_mesg (ITC_mesg *M);
+    virtual void on_open_midi() = 0;
+    virtual void on_close_midi() = 0;
+    virtual void on_terminate() = 0;
 
-    Lfq_u32        *_qnote; 
+protected:
+    const char     *_appname;
+
+private:
+    Lfq_u32        *_qnote;
     Lfq_u8         *_qmidi; 
     uint16_t       *_midimap;
-    const char     *_appname;
-#ifdef __linux__
-    snd_seq_t      *_handle;
-#endif
-#ifdef __APPLE__
-    MIDIClientRef   _handle;
-#endif
     int             _client;
     int             _ipport;
-    int             _opport;
 };
 
 

source/imidi_alsa.h
@@ -1,0 +1,43 @@
+// ----------------------------------------------------------------------------
+//
+//  Copyright (C) 2003-2013 Fons Adriaensen <fons@linuxaudio.org>
+//    
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// ----------------------------------------------------------------------------
+
+
+#ifndef __IMIDI_ALSA_H
+#define __IMIDI_ALSA_H
+
+#include "imidi.h"
+#include <alsa/asoundlib.h>
+
+class Imidi_alsa : public Imidi
+{
+public:
+    Imidi_alsa (Lfq_u32 *qnote, Lfq_u8 *qmidi, uint16_t *midimap, const char *appname);
+
+private:
+    void thr_main (void) override;
+
+protected:
+    void on_open_midi (void) override;
+    void on_close_midi (void) override;
+    void on_terminate() override;
+    
+    snd_seq_t      *_handle;
+};
+
+#endif

source/imidi_coremidi.cc
@@ -1,0 +1,129 @@
+// ----------------------------------------------------------------------------
+//
+//  Copyright (C) 2003-2013 Fons Adriaensen <fons@linuxaudio.org>
+//    
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// ----------------------------------------------------------------------------
+
+
+#include "imidi_coremidi.h"
+
+Imidi_coremidi::Imidi_coremidi (Lfq_u32 *qnote, Lfq_u8 *qmidi, uint16_t *midimap, const char *appname) : Imidi(qnote, qmidi, midimap, appname),
+    _handle(NULL),
+    _endpoint(NULL)
+{
+}
+
+
+void Imidi_coremidi::on_terminate (void)
+{
+    put_event (EV_EXIT, 1);
+}
+
+
+void Imidi_coremidi::thr_main (void)
+{
+    open_midi ();
+    get_event (1 << EV_EXIT);
+    close_midi ();
+    send_event (EV_EXIT, 1);
+}
+
+
+void Imidi_coremidi::on_open_midi (void)
+{
+    CFStringRef name = CFStringCreateWithCString(0, _appname, kCFStringEncodingUTF8);
+    MIDIClientCreate(name, NULL, NULL, &_handle);
+    MIDIEndpointRef dest = NULL;
+    MIDIDestinationCreate(_handle, name, &coremidi_callback, this, &dest);
+}
+
+void Imidi_coremidi::on_close_midi (void)
+{
+    MIDIEndpointDispose(_endpoint);
+    _endpoint = NULL;
+    MIDIClientDispose(_handle);
+    _handle = NULL;
+}
+
+void Imidi_coremidi::coremidi_callback(const MIDIPacketList *pktlist, void *refCon, void *connRefCon)
+{
+    Imidi_coremidi* pInstance = static_cast<Imidi_coremidi*>(refCon);
+    const MIDIPacket* packet = pktlist->packet;
+    bool skip = false;
+    for(int i = 0; i < pktlist->numPackets; ++i) {
+        const Byte* pByte = packet->data;
+        while(pByte - packet->data < packet->length) {
+            if(skip) {
+                if(*pByte == 0xf7)
+                    skip = false;
+                ++pByte;
+                continue;
+            }
+            Imidi::MidiEvent ev = { 0 };
+            int upper = *pByte & 0xf0,
+                lower = *pByte & 0x0f;
+            ev.note.channel = lower;
+            switch(upper) {
+                case 0x80: // note off
+                    ev.type = SND_SEQ_EVENT_NOTEOFF;
+                    ev.note.note = *++pByte;
+                    ev.note.velocity = *++pByte;
+                    break;
+                case 0x90: // note on
+                    ev.type = SND_SEQ_EVENT_NOTEON;
+                    ev.note.note = *++pByte;
+                    ev.note.velocity = *++pByte;
+                    break;
+                case 0xb0: // control change
+                    ev.type = SND_SEQ_EVENT_CONTROLLER;
+                    ev.control.param = *++pByte;
+                    ev.control.value = *++pByte;
+                    break;
+                case 0xd0: // program change
+                    ev.type = SND_SEQ_EVENT_PGMCHANGE;
+                    ev.control.value = *++pByte;
+                    break;
+                case 0xa0: // after touch
+                case 0xe0: // pitch bend change
+                    ev.type = SND_SEQ_EVENT_NONE;
+                    pByte += 2;
+                    break;
+                case 0xc0: // channel pressure
+                    ev.type = SND_SEQ_EVENT_NONE;
+                    pByte += 1;
+                    break;
+                case 0xf0: // system
+                    ev.type = SND_SEQ_EVENT_NONE;
+                    switch(lower) {
+                        case 0x00: // sysex
+                            skip = true;
+                            break;
+                        case 0x01: // midi time point quarter frame
+                        case 0x03: // song position pointer
+                            pByte += 1;
+                            break;
+                        case 0x02: // song select
+                            pByte += 2;
+                            break;
+                        // remaining system events have no data
+                    }
+            }
+            ++pByte;
+            pInstance->proc_midi_event(ev);
+        }
+        packet = MIDIPacketNext(packet);
+    }
+}

source/imidi_coremidi.h
@@ -1,0 +1,46 @@
+// ----------------------------------------------------------------------------
+//
+//  Copyright (C) 2003-2013 Fons Adriaensen <fons@linuxaudio.org>
+//    
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// ----------------------------------------------------------------------------
+
+
+#ifndef __IMIDI_COREMIDI_H
+#define __IMIDI_COREMIDI_H
+
+#include "imidi.h"
+#include <CoreMIDI/MIDIServices.h>
+
+class Imidi_coremidi : public Imidi
+{
+public:
+
+    Imidi_coremidi (Lfq_u32 *qnote, Lfq_u8 *qmidi, uint16_t *midimap, const char *appname);
+
+private:
+    void thr_main (void) override;
+
+    void on_open_midi (void) override;
+    void on_close_midi (void) override;
+    void on_terminate() override;
+    
+    static void coremidi_callback(const MIDIPacketList *, void *, void*);
+
+    MIDIClientRef   _handle;
+    MIDIEndpointRef _endpoint;
+};
+
+#endif

source/main.cc
@@ -28,7 +28,11 @@
 #include <clthreads.h>
 #include <dlfcn.h>
 #include "audio.h"
-#include "imidi.h"
+#if __linux__
+# include "imidi_alsa.h"
+#elif __APPLE__
+# include "imidi_coremidi.h"
+#endif
 #include "model.h"
 #include "slave.h"
 #include "iface.h"
@@ -218,7 +222,11 @@ int main (int ac, char *av [])
     audio->init_jack (s_val, B_opt, &midi_queue);
 #endif
     model = new Model (&comm_queue, &midi_queue, audio->midimap (), audio->appname (), S_val, I_val, W_val, u_opt);
-    imidi = new Imidi (&note_queue, &midi_queue, audio->midimap (), audio->appname ());
+#if __linux__
+    imidi = new Imidi_alsa (&note_queue, &midi_queue, audio->midimap (), audio->appname ());
+#elif __APPLE__
+    imidi = new Imidi_coremidi (&note_queue, &midi_queue, audio->midimap (), audio->appname ());
+#endif
     slave = new Slave ();
     iface = so_create (ac, av);
 

commit 176bed075e5226abccbb9741f511cdf162384cea

fix Alsa midi (broken by 16c798c)

source/imidi_alsa.cc
@@ -124,5 +124,7 @@ void Imidi_alsa::proc_midi (void)
                 ev.control.value = E->data.control.value;
                 break;
         }
+
+        proc_midi_event(ev);
     }
 }